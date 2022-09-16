ad9f1841bc789bd8da2e5443f4cf9b9c69058c31
reza <reza138398123@gmail.com>
2022-09-16 12:01:27 +0430

    fix(db_user.py): fix bug
    add user validate

--- database/db_user.py
+++ database/db_user.py
@@ -1,13 +1,23 @@
 from .setting import db
 from schemas import UserBase
+from fastapi.exceptions import HTTPException
+from fastapi import status
 
 
 def db_create_user(request: UserBase):
-    user_dict = {
-        'username': request.username,
-        'password': request.password,
-        'email': request.email
-    }
-    db.users.insert_one(user_dict)
 
-    return {'status': 'ok', 'username': request.username, 'email': request.email}
+    # email validate    
+    if '@' not in request.email:
+        raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE, detail=f"your email {request.email} not valid")
+
+    # password validate
+    elif len(request.password) < 4:
+        raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE, detail="your password is short")
+ 
+    else:
+        user_dict = {
+            'username': request.username,
+            'password': request.password,
+            'email': request.email}
+        db.users.insert_one(user_dict)
+        return {'status': 'ok', 'username': request.username, 'email': request.email}
